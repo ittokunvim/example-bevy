e884af9fb2aa5bad4166f2bef8bd3f078becedc8
ittokun <73953416+ittokun@users.noreply.github.com>
2023-06-02 23:27:54 +0900

    feat: :sparkles: Add ball velocity
    Issue: #3

--- examples/click.rs
+++ examples/click.rs
@@ -3,12 +3,13 @@ use rand::distributions::{Distribution, Uniform};
 
 const BALL_COUNT: u32 = 100;
 const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+const BALL_SPEED: f32 = 400.0;
 
 const WALL_THICKNESS: f32 = 10.0;
-const LEFT_WALL: f32 = -450.;
-const RIGHT_WALL: f32 = 450.;
-const BOTTOM_WALL: f32 = -300.;
-const TOP_WALL: f32 = 300.;
+const LEFT_WALL: f32 = -450.0;
+const RIGHT_WALL: f32 = 450.0;
+const BOTTOM_WALL: f32 = -300.0;
+const TOP_WALL: f32 = 300.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const BALL_COLOR: Color = Color::rgb(0.9, 0.3, 0.3);
@@ -20,9 +21,13 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
         .add_startup_system(setup)
+        .add_system(apply_velocity)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
 
 #[derive(Bundle)]
 struct WallBundle {
@@ -97,17 +102,30 @@ fn setup(
     let mut rng = rand::thread_rng();
     let die_width = Uniform::from(LEFT_WALL + BALL_SIZE.x..RIGHT_WALL - BALL_SIZE.x);
     let die_height = Uniform::from(BOTTOM_WALL + BALL_SIZE.y..TOP_WALL - BALL_SIZE.y);
+    let die_velocity = Uniform::from(-0.5..0.5);
 
     for _ in 0..BALL_COUNT {
-        let offset_x = die_width.sample(&mut rng);
-        let offset_y = die_height.sample(&mut rng);
+        let ball_pos_x = die_width.sample(&mut rng);
+        let ball_pos_y = die_height.sample(&mut rng);
+        let ball_velocity_x = die_velocity.sample(&mut rng);
+        let ball_velocity_y = die_velocity.sample(&mut rng);
 
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::default().into()).into(),
-            material: materials.add(ColorMaterial::from(BALL_COLOR)),
-            transform: Transform::from_translation(Vec3::new(offset_x, offset_y, 1.0))
-                .with_scale(BALL_SIZE),
-            ..default()
-        });
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::default().into()).into(),
+                material: materials.add(ColorMaterial::from(BALL_COLOR)),
+                transform: Transform::from_translation(Vec3::new(ball_pos_x, ball_pos_y, 1.0))
+                    .with_scale(BALL_SIZE),
+                ..default()
+            },
+            Velocity(Vec2::new(ball_velocity_x, ball_velocity_y) * BALL_SPEED),
+        ));
+    }
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * time_step.period.as_secs_f32();
+        transform.translation.y += velocity.y * time_step.period.as_secs_f32();
     }
 }
