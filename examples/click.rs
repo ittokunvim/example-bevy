6eafee33320d506d26cd319938f8853cb95d8c19
ittokun <73953416+ittokun@users.noreply.github.com>
2023-06-02 21:37:09 +0900

    feat: :sparkles: Add setup
    Issue: #3

--- examples/click.rs
+++ examples/click.rs
@@ -1,12 +1,113 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use rand::distributions::{Distribution, Uniform};
+
+const BALL_COUNT: u32 = 100;
+const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+
+const WALL_THICKNESS: f32 = 10.0;
+const LEFT_WALL: f32 = -450.;
+const RIGHT_WALL: f32 = 450.;
+const BOTTOM_WALL: f32 = -300.;
+const TOP_WALL: f32 = 300.;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+const BALL_COLOR: Color = Color::rgb(0.9, 0.3, 0.3);
+const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc)
         .run();
+}
+
+#[derive(Bundle)]
+struct WallBundle {
+    sprite_bundle: SpriteBundle,
+}
+
+enum WallLocation {
+    Left,
+    Right,
+    Bottom,
+    Top,
+}
+
+impl WallLocation {
+    fn position(&self) -> Vec2 {
+        match self {
+            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
+            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
+            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
+            WallLocation::Top => Vec2::new(0., TOP_WALL),
+        }
+    }
+
+    fn size(&self) -> Vec2 {
+        let area_height = TOP_WALL - BOTTOM_WALL;
+        let area_width = RIGHT_WALL - LEFT_WALL;
+
+        match self {
+            WallLocation::Left | WallLocation::Right => {
+                Vec2::new(WALL_THICKNESS, area_height + WALL_THICKNESS)
+            }
+            WallLocation::Bottom | WallLocation::Top => {
+                Vec2::new(area_width + WALL_THICKNESS, WALL_THICKNESS)
+            }
+        }
+    }
+}
+
+impl WallBundle {
+    fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                transform: Transform {
+                    translation: location.position().extend(0.0),
+                    scale: location.size().extend(0.0),
+                    ..default()
+                },
+                ..default()
+            },
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Walls
+    commands.spawn(WallBundle::new(WallLocation::Left));
+    commands.spawn(WallBundle::new(WallLocation::Right));
+    commands.spawn(WallBundle::new(WallLocation::Bottom));
+    commands.spawn(WallBundle::new(WallLocation::Top));
+
+    let mut rng = rand::thread_rng();
+    let die_width = Uniform::from(LEFT_WALL + BALL_SIZE.x..RIGHT_WALL - BALL_SIZE.x);
+    let die_height = Uniform::from(BOTTOM_WALL + BALL_SIZE.y..TOP_WALL - BALL_SIZE.y);
+
+    for _ in 0..BALL_COUNT {
+        let offset_x = die_width.sample(&mut rng);
+        let offset_y = die_height.sample(&mut rng);
+
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(BALL_COLOR)),
+            transform: Transform::from_translation(Vec3::new(offset_x, offset_y, 1.0))
+                .with_scale(BALL_SIZE),
+            ..default()
+        });
+    }
 }
