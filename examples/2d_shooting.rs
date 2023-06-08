4bff03825f7813007f00363849cbd474ea946b7c
ittokun <73953416+ittokun@users.noreply.github.com>
2023-06-09 02:15:50 +0900

    feat: :sparkles: Add Player setup
    Issue: #5

--- examples/2d_shooting.rs
+++ examples/2d_shooting.rs
@@ -1,8 +1,11 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
-const WINDOW_SIZE: Vec2 = Vec2::new(900.0, 600.0);
+const WINDOW_SIZE: Vec2 = Vec2::new(700.0, 700.0);
+
+const GAP_BETWEEN_PLAYER_AND_FLOOR: f32 = 40.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
+const PLAYER_COLOR: Color = Color::rgb(0.3, 0.9, 0.3);
 
 fn main() {
     App::new()
@@ -15,7 +18,32 @@ fn main() {
         }))
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
 
+#[derive(Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Player
+    let player_y = -WINDOW_SIZE.y / 2. + GAP_BETWEEN_PLAYER_AND_FLOOR;
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::RegularPolygon::new(30., 3).into()).into(),
+            material: materials.add(ColorMaterial::from(PLAYER_COLOR)),
+            transform: Transform::from_translation(Vec3::new(0., player_y, 0.)),
+            ..default()
+        },
+        Player,
+    ));
+}
