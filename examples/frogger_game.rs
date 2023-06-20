68459a185358832f08b62bfadfbf60019819629e
ittokun <73953416+ittokun@users.noreply.github.com>
2023-06-21 03:22:22 +0900

    feat: :heavy_plus_sign: frogger_game - setup tile
    Issue: #6

--- examples/frogger_game.rs
+++ examples/frogger_game.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
+const BOARD_SIZE_I: usize = 14;
+const BOARD_SIZE_J: usize = 21;
+
+const RESET_FOCUS: [f32; 3] = [
+    BOARD_SIZE_I as f32 / 2.0,
+    0.0,
+    BOARD_SIZE_J as f32 / 2.0 - 0.5,
+];
+
 const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 
 fn main() {
@@ -7,6 +16,45 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(
+            -(BOARD_SIZE_I as f32 / 2.0),
+            2.0 * BOARD_SIZE_J as f32 / 3.0,
+            BOARD_SIZE_J as f32 / 2.0 - 0.5,
+        )
+        .looking_at(Vec3::from(RESET_FOCUS), Vec3::Y),
+        ..default()
+    });
+
+    // Light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 10.0, 4.0),
+        point_light: PointLight {
+            intensity: 3000.0,
+            shadows_enabled: true,
+            range: 30.0,
+            ..default()
+        },
+        ..default()
+    });
+
+    // Board
+    let cell_scene = asset_server.load("models/Frogger/tile.glb#Scene0");
+
+    (0..BOARD_SIZE_I).for_each(|i| {
+        (0..BOARD_SIZE_J).for_each(|j| {
+            commands.spawn(SceneBundle {
+                transform: Transform::from_xyz(i as f32, 0.0, j as f32),
+                scene: cell_scene.clone(),
+                ..default()
+            });
+        });
+    });
 }
