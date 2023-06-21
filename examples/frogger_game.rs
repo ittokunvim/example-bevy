b7b5533a61829809fc3f625cb20b62d87653c5be
ittokun <73953416+ittokun@users.noreply.github.com>
2023-06-22 03:52:24 +0900

    feat: :heavy_plus_sign: frogger_game - setup player
    Issue: #6

--- examples/frogger_game.rs
+++ examples/frogger_game.rs
@@ -1,13 +1,11 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 
 const BOARD_SIZE_I: usize = 14;
 const BOARD_SIZE_J: usize = 21;
 
-const RESET_FOCUS: [f32; 3] = [
-    BOARD_SIZE_I as f32 / 2.0,
-    0.0,
-    BOARD_SIZE_J as f32 / 2.0 - 0.5,
-];
+const PLAYER_INITIAL_POSITION: Vec3 = Vec3::new(0.0, 0.0, BOARD_SIZE_J as f32 / 2.0 - 0.5);
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 
@@ -25,11 +23,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(
-            -(BOARD_SIZE_I as f32 / 2.0),
-            2.0 * BOARD_SIZE_J as f32 / 3.0,
-            BOARD_SIZE_J as f32 / 2.0 - 0.5,
+            PLAYER_INITIAL_POSITION.x - 2.8,
+            PLAYER_INITIAL_POSITION.y + 3.0,
+            PLAYER_INITIAL_POSITION.z + 3.5,
         )
-        .looking_at(Vec3::from(RESET_FOCUS), Vec3::Y),
+        .looking_at(Vec3::from(PLAYER_INITIAL_POSITION), Vec3::Y),
         ..default()
     });
 
@@ -51,10 +49,23 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     (0..BOARD_SIZE_I).for_each(|i| {
         (0..BOARD_SIZE_J).for_each(|j| {
             commands.spawn(SceneBundle {
-                transform: Transform::from_xyz(i as f32, 0.0, j as f32),
+                transform: Transform::from_xyz(i as f32, -0.2, j as f32),
                 scene: cell_scene.clone(),
                 ..default()
             });
         });
+    });
+
+    // Player
+    let player_asset = asset_server.load("models/Frogger/gekota.glb#Scene0");
+
+    commands.spawn(SceneBundle {
+        transform: Transform {
+            translation: PLAYER_INITIAL_POSITION,
+            rotation: Quat::from_rotation_y(-PI / 2.),
+            ..default()
+        },
+        scene: player_asset,
+        ..default()
     });
 }
