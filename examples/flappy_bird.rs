5d5aa4ecdc6cfb78098eb42e7885271aa475f4b7
ittokun <73953416+ittokun@users.noreply.github.com>
2023-07-02 20:59:02 +0900

    feat: :sparkles: flappy_bird - Add jump_player
    Issue: #7

--- examples/flappy_bird.rs
+++ examples/flappy_bird.rs
@@ -4,6 +4,7 @@ use bevy::sprite::MaterialMesh2dBundle;
 const WINDOW_SIZE: Vec2 = Vec2::new(700.0, 700.0);
 
 const PLAYER_SIZE: f32 = 25.0;
+const PLAYER_JUMP: f32 = 50.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const PLAYER_COLOR: Color = Color::rgb(0.1, 0.8, 0.1);
@@ -20,9 +21,13 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
         .add_startup_system(setup)
+        .add_system(jump_player)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
+
+#[derive(Component)]
+struct Player;
 
 fn setup(
     mut commands: Commands,
@@ -42,5 +47,17 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
             ..default()
         },
+        Player,
     ));
+}
+
+fn jump_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+) {
+    let mut player_transform = player_query.single_mut();
+
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        player_transform.translation.y += PLAYER_JUMP;
+    }
 }
