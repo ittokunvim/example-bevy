98c32d682547e60cd2b35c006df781ae3f1825ec
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-08-03 02:27:55 +0900

    feat: :sparkles: catch game - Add press_any_key
    Issue: #11

--- examples/catch_game.rs
+++ examples/catch_game.rs
@@ -1,8 +1,13 @@
+use bevy::input::keyboard::KeyboardInput;
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 
 const WINDOW_SIZE: Vec2 = Vec2::new(800.0, 600.0);
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
+const PRESSANYKEY_FONT_SIZE: f32 = 30.0;
+const PRESSANYKEY_COLOR: Color = Color::rgb(0.5, 0.5, 0.5);
+const PRESSANYKEY_TEXT_PADDING: f32 = 20.0;
 
 const PLAYER_SIZE: Vec3 = Vec3::new(25.0, 25.0, 0.0);
 const PLAYER_COLOR: Color = Color::rgb(0.1, 0.8, 0.1);
@@ -28,17 +33,41 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
         .add_systems(Startup, setup)
+        .add_systems(Update, press_any_key.run_if(in_state(AppState::MainMenu)))
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
+
+#[derive(Component)]
+struct PressAnyKey;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     // Camera
     commands.spawn(Camera2dBundle::default());
+
+    // Press any key
+    commands.spawn((
+        TextBundle::from_section(
+            "Press Any Key ...",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: PRESSANYKEY_FONT_SIZE,
+                color: PRESSANYKEY_COLOR,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(PRESSANYKEY_TEXT_PADDING),
+            right: Val::Px(PRESSANYKEY_TEXT_PADDING),
+            ..default()
+        }),
+        PressAnyKey,
+    ));
 
     // Player
     let player_y = -WINDOW_SIZE.y / 2.0 + PLAYER_SIZE.y;
@@ -53,4 +82,20 @@ fn setup(
         },
         ..default()
     });
+}
+
+fn press_any_key(
+    mut keyboard_event: EventReader<KeyboardInput>,
+    pressanykey_query: Query<Entity, With<PressAnyKey>>,
+    mut commands: Commands,
+    mut now_state: ResMut<State<AppState>>,
+    mut inkey: ResMut<Input<KeyCode>>,
+) {
+    for _event in keyboard_event.iter() {
+        let pressanykey_entity = pressanykey_query.single();
+        commands.entity(pressanykey_entity).despawn();
+
+        *now_state = State::new(AppState::InGame);
+        inkey.reset_all();
+    }
 }
