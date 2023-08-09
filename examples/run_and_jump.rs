09af01255927fea111154350ea205defa65d6675
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-08-10 02:36:39 +0900

    feat: :sparkles: run and jump - Add setup
    Issue: #16

---
+++ examples/run_and_jump.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+const WINDOW_SIZE: Vec2 = Vec2::new(800.0, 600.0);
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug, Default, States)]
+enum AppState {
+    #[default]
+    MainMenu,
+    InGame,
+    GameOver,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WINDOW_SIZE.into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_state::<AppState>()
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .add_systems(Startup, setup_camera)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+}
