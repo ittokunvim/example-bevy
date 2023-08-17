609851dd11e3edd49c0f20e8a7a083c6f69605ce
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-08-18 03:49:07 +0900

    Revert "feat: :sparkles: run and jump - Add setup_ldtk"
    This reverts commit bd65f4cb06b9225e2207677d95ac1d0aacbd6811.
    
    Issue: #19

--- examples/run_and_jump.rs
+++ examples/run_and_jump.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 
-use bevy_ecs_ldtk::prelude::*;
-
 const WINDOW_SIZE: Vec2 = Vec2::new(800.0, 600.0);
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
@@ -15,50 +13,22 @@ enum AppState {
 
 fn main() {
     App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        resolution: WINDOW_SIZE.into(),
-                        ..default()
-                    }),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest()),
-        )
-        .add_plugins(LdtkPlugin)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WINDOW_SIZE.into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_state::<AppState>()
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
-        .insert_resource(LevelSelection::Index(0))
-        .insert_resource(LdtkSettings {
-            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
-                load_level_neighbors: true,
-            },
-            set_clear_color: SetClearColor::FromLevelBackground,
-            ..default()
-        })
-        .register_ldtk_entity::<LdtkBundle>("LdtkEntityIdentifier")
         .add_systems(Startup, setup_camera)
-        .add_systems(Startup, setup_ldtk)
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
-}
-
-#[derive(Bundle, LdtkEntity)]
-struct LdtkBundle {
-    #[sprite_sheet_bundle]
-    sprite_sheet: SpriteSheetBundle,
 }
 
 fn setup_camera(mut commands: Commands) {
     // Camera
     commands.spawn(Camera2dBundle::default());
-}
-
-fn setup_ldtk(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("ldtk/run_and_jump.ldtk"),
-        ..default()
-    });
 }
