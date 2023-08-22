a71243f863ea09206a46224fd8594acdd740d3b3
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-08-23 01:45:36 +0900

    feat: :sparkles: run and jump - Add focus_camera_on_player
    Issue: #16

--- examples/run_and_jump/main.rs
+++ examples/run_and_jump/main.rs
@@ -13,6 +13,8 @@ const PLAYER_SIZE: Vec3 = Vec3::new(25.0, 25.0, 0.0);
 const PLAYER_COLOR: Color = Color::rgb(0.1, 0.8, 0.1);
 const PLAYER_SPEED: f32 = 100.0;
 
+const CAMERA_FOCUS_OFFSET: f32 = -200.0;
+
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug, Default, States)]
 enum AppState {
     #[default]
@@ -37,6 +39,7 @@ fn main() {
         .add_systems(Startup, setup_tilemap)
         .add_systems(Startup, setup_player)
         .add_systems(Update, apply_velocity)
+        .add_systems(Update, focus_camera_on_player)
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
@@ -117,5 +120,17 @@ fn setup_player(
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * time_step.period.as_secs_f32();
+    }
+}
+
+fn focus_camera_on_player(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+) {
+    let player_transform = player_query.single();
+    let mut camera_transform = camera_query.single_mut();
+
+    if player_transform.translation.x > CAMERA_FOCUS_OFFSET {
+        camera_transform.translation.x = player_transform.translation.x - CAMERA_FOCUS_OFFSET;
     }
 }
