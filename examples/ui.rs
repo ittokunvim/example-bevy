4c45dda43a7065ae28619c1e17f03439b665afd8
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-06-19 15:40:40 +0900

    add setup to ui.rs
    Issue: #24

--- examples/ui.rs
+++ examples/ui.rs
@@ -9,6 +9,26 @@ const LOG_FILTER: &str = "info,wgpu_core=warn,wgpu_hal=warn,ui=debug";
 const PATH_FONT: &str = "fonts/misaki_gothic.ttf";
 const PATH_IMAGE_FONTAWESOME: &str = "images/fontawesome.png";
 
+const ROOT_WIDTH: Val = Val::Percent(100.0);
+const ROOT_HEIGHT: Val = Val::Percent(100.0);
+
+const BOARD_SIZE: Vec2 = Vec2::new(360.0, 270.0);
+const BOARD_LEFT: Val = Val::Px(WINDOW_SIZE.x / 2.0 - BOARD_SIZE.x / 2.0);
+const BOARD_TOP: Val = Val::Px(WINDOW_SIZE.y / 2.0 - BOARD_SIZE.y / 2.0);
+const BOARD_PADDING: Val = Val::Px(16.0);
+const BOARD_COLOR: Color = Color::srgb(0.8, 0.8, 0.8);
+
+const ICON_SIZE: Vec2 = Vec2::new(24.0, 24.0);
+const BUTTON_WIDTH: Val = Val::Px(ICON_SIZE.x * 2.0);
+const BUTTON_HEIGHT: Val = Val::Px(ICON_SIZE.y * 2.0);
+
+const TEXT_FONT_SIZE: f32 = 24.0;
+const TEXT_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
+
+const BORDER_SIZE: Val = Val::Px(4.0);
+const BORDER_COLOR: Color = Color::srgb(0.5, 0.5, 1.0);
+const BORDER_RADIUS: Val = Val::Px(10.0);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins
@@ -28,6 +48,81 @@ fn main() {
             })
         )
         .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_systems(Startup, setup)
         .run();
+}
+
+/// UIのセットアップを行う関数
+/// 構造：
+/// * root
+///   * board
+///     * title
+///     * button
+///       * icon
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    info_once!("setup");
+
+    // カメラを生成
+    commands.spawn(Camera2d::default());
+
+    let font = asset_server.load(PATH_FONT);
+    let image = asset_server.load(PATH_IMAGE_FONTAWESOME);
+    commands.spawn((
+       Node {
+            width: ROOT_WIDTH,
+            height: ROOT_HEIGHT,
+            ..Default::default()
+        },
+        children![(
+            Node {
+                width: Val::Px(BOARD_SIZE.x),
+                height: Val::Px(BOARD_SIZE.y),
+                border: UiRect::all(BORDER_SIZE),
+                position_type: PositionType::Absolute,
+                left: BOARD_LEFT,
+                top: BOARD_TOP,
+                padding: UiRect::all(BOARD_PADDING),
+                justify_content: JustifyContent::SpaceBetween,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
+                ..Default::default()
+            },
+            BackgroundColor(BOARD_COLOR),
+            BorderColor(BORDER_COLOR),
+            BorderRadius::all(BORDER_RADIUS),
+            children![(
+                Text::new(GAMETITLE),
+                TextFont {
+                    font: font.clone(),
+                    font_size: TEXT_FONT_SIZE,
+                    ..Default::default()
+                },
+                TextColor(TEXT_COLOR),
+            ),(
+                Button,
+                Node {
+                    width: BUTTON_WIDTH,
+                    height: BUTTON_HEIGHT,
+                    border: UiRect::all(BORDER_SIZE),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                BorderColor(BORDER_COLOR),
+                BorderRadius::all(BORDER_RADIUS),
+                children![(
+                    ImageNode::new(image.clone()),
+                    Node {
+                        width: Val::Px(ICON_SIZE.x),
+                        height: Val::Px(ICON_SIZE.y),
+                        ..Default::default()
+                    },
+                )],
+            )],
+        )]
+    ));
 }
 
