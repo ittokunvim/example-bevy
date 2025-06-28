2e6e876fd00c683316a9fcfef68af67220ff2343
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-06-28 23:38:22 +0900

    feat: Add setup
    Issue: #28

--- examples/spritesheet.rs
+++ examples/spritesheet.rs
@@ -7,6 +7,11 @@ use bevy::{
 const GAMETITLE: &str = "スプライトシート";
 const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const LOG_FILTER: &str = "info,wgpu_core=warn,wgpu_hal=warn,spritesheet=debug";
+const PATH_IMAGES_SPRITESHEET: &str = "images/spritesheet.png";
+const IMAGE_SIZE: u32 = 32;
+const SIZE: f32 = 64.0;
+const IMAGE_COLUMN: u32 = 4;
+const IMAGE_ROW: u32 = 5;
 
 fn main() {
     App::new()
@@ -31,6 +36,40 @@ fn main() {
             })
         )
         .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
+        .add_systems(Startup, setup)
         .run()
     ;
+}
+
+fn setup(
+    mut commands: Commands,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    asset_server: Res<AssetServer>,
+) {
+    info_once!("setup");
+
+    // カメラを生成
+    commands.spawn(Camera2d::default());
+
+    // スプライトシートを生成
+    let texture = asset_server.load(PATH_IMAGES_SPRITESHEET);
+    let layout = TextureAtlasLayout::from_grid(
+        UVec2::splat(IMAGE_SIZE),
+        IMAGE_COLUMN,
+        IMAGE_ROW,
+        None,
+        None,
+    );
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    commands.spawn((
+        Sprite {
+            image: texture.clone(),
+            texture_atlas: Some(TextureAtlas {
+                layout: texture_atlas_layout.clone(),
+                index: 0,
+            }),
+            custom_size: Some(Vec2::splat(SIZE)),
+            ..Default::default()
+        },
+    ));
 }
