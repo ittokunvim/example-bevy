4d03e0ed00f2d34f9b91523ead1449255b814fad
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-06-28 23:50:05 +0900

    fix: Add animation
    Issue: #28

--- examples/spritesheet.rs
+++ examples/spritesheet.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::{
     prelude::*,
     log::LogPlugin,
@@ -12,6 +14,8 @@ const IMAGE_SIZE: u32 = 32;
 const SIZE: f32 = 64.0;
 const IMAGE_COLUMN: u32 = 4;
 const IMAGE_ROW: u32 = 5;
+const IDLE_INDICES: (usize, usize) = (0, 3);
+const IDLE_FPS: u8 = 4;
 
 fn main() {
     App::new()
@@ -37,8 +41,32 @@ fn main() {
         )
         .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
         .add_systems(Startup, setup)
+        .add_systems(Update, animation)
         .run()
     ;
+}
+
+#[derive(Component, Debug)]
+struct AnimationConfig {
+    first_sprite_index: usize,
+    last_sprite_index: usize,
+    fps: u8,
+    frame_timer: Timer,
+}
+
+impl AnimationConfig {
+    fn new(first: usize, last: usize, fps: u8) -> Self {
+        Self {
+            first_sprite_index: first,
+            last_sprite_index: last,
+            fps,
+            frame_timer: Self::timer_from_fps(fps),
+        }
+    }
+
+    fn timer_from_fps(fps: u8) -> Timer {
+        Timer::new(Duration::from_secs_f32(1.0 / (fps as f32)), TimerMode::Repeating)
+    }
 }
 
 fn setup(
@@ -61,15 +89,39 @@ fn setup(
         None,
     );
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let animation_config = AnimationConfig::new(IDLE_INDICES.0, IDLE_INDICES.1, IDLE_FPS);
     commands.spawn((
         Sprite {
             image: texture.clone(),
             texture_atlas: Some(TextureAtlas {
                 layout: texture_atlas_layout.clone(),
-                index: 0,
+                index: IDLE_INDICES.0,
             }),
             custom_size: Some(Vec2::splat(SIZE)),
             ..Default::default()
         },
+        animation_config,
     ));
+}
+
+fn animation(
+    mut query: Query<(&mut AnimationConfig, &mut Sprite), With<AnimationConfig>>,
+    time: Res<Time>,
+) {
+    info_once!("animation");
+
+    for (mut config, mut sprite) in &mut query {
+        config.frame_timer.tick(time.delta());
+
+        if config.frame_timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                if atlas.index == config.last_sprite_index {
+                    atlas.index = config.first_sprite_index;
+                } else {
+                    atlas.index += 1;
+                    config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+                }
+            }
+        }
+    }
 }
